c1e17e8cbf6f05ae39b66118084a584efcc1db4d
runqingc <cairunqing02@outlook.com>
2023-10-15 11:35:39 -0500

    updated P8_14

--- src/main/java/edu/uchicago/gerber/_03objects/P8_14/Driver.java
+++ src/main/java/edu/uchicago/gerber/_03objects/P8_14/Driver.java
@@ -6,28 +6,24 @@ import java.io.IOException;
 
 public class Driver {
 
-    public class CountryReader {
+    public static void main(String[] args) {
+        String filePath = "P8_14/countries.txt";
 
-        public static void main(String[] args) {
-            String filePath = "countries.txt";
-
-            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    String[] parts = line.split(" ");
-                    if (parts.length == 3) {
-                        String countryName = parts[0];
-                        String population = parts[1];
-                        String area = parts[2];
-                        System.out.println("Name: " + countryName + "; Population: " + population + "; Area: " + area);
-                    }
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length == 3) {
+                    String countryName = parts[0];
+                    long population = Long.parseLong(parts[1]);
+                    double area = Double.parseDouble(parts[2]);
+                    Country country= new Country(countryName, population, area);
+                    System.out.println("Name: " + country.getName() + "; Population: " + country.getPopulation()
+                            + "; Area: " + country.getArea()+ " ;Density: " + country.density());
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
-
-
 }
