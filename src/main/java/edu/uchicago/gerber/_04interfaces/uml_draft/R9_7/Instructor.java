6a22b007d35da659a666c1be55495b7c45c1bd8a
runqingc <cairunqing02@outlook.com>
2023-10-22 15:16:21 -0500

    modified uml

---
+++ src/main/java/edu/uchicago/gerber/_04interfaces/uml_draft/R9_7/Instructor.java
@@ -0,0 +1,4 @@
+package edu.uchicago.gerber._04interfaces.uml_draft.R9_7;
+
+public class Instructor extends Employee{
+}
---
+++ src/main/java/edu/uchicago/gerber/_04interfaces/uml_draft/R9_8/Vehicle.java
@@ -0,0 +1,4 @@
+package edu.uchicago.gerber._04interfaces.uml_draft.R9_8;
+
+public class Vehicle {
+}
