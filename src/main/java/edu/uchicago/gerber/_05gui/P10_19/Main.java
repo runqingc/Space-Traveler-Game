47c077fada8c1049e4048ac57424ed07b81e2b8d
runqingc <cairunqing02@outlook.com>
2023-10-27 15:34:25 -0500

    updated P10_19

---
+++ src/main/java/edu/uchicago/gerber/_05gui/P10_19/Main.java
@@ -0,0 +1,18 @@
+package P10_19;
+
+import javax.swing.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        BillFrame frame = new BillFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+
+
+    }
+}
