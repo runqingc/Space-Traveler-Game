4283defa24cd638f8d5971ff0f14979375e593c6
runqingc <cairunqing02@outlook.com>
2023-10-28 11:50:43 -0500

    updated P11.9

---
+++ src/main/java/edu/uchicago/gerber/_05gui/P11_9/CircleViewer.java
@@ -0,0 +1,13 @@
+package P11_9;
+
+public class CircleViewer {
+
+    public static void main(String[] args){
+
+        CircleFrame circleFrame = new CircleFrame();
+        circleFrame.setVisible(true);
+
+
+    }
+
+}
